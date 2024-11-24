708f53dc0cccbd3660dabe7305b8e045f8e4347b
Raphaël Pierre <pierre.raphael@gmail.com>
2024-11-24 17:32:24 +0100

    defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/byteflow-contact/',
   
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react']
     
   },
-base: 'byteflow-contact'
+
 });
