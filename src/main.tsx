39673b5c97daeb451ed25cb35134de9bc37925b3
Raphaël Pierre <pierre.raphael@gmail.com>
2024-11-24 17:30:00 +0100

    remove BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={"/byteflow-contact/"}></BrowserRouter>
     <App />
   </StrictMode>
 );
-
