659985a77895c219232531d18f877d7e0f5f2fc6
raphaelpierre <pierre.raphael@gmail.com>
2024-11-24 17:44:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 //
 export default defineConfig({
   base: '/byteflow-contact/',
+  homepage: 'http://.github.io/raphaelpierre'
   
   plugins: [react()],
   optimizeDeps: {
